d01714835533ce9f5a547d7b0d58299ae8d60b24
ivan-aricent <ivan.mandal@aricent.com>
2017-06-18 21:57:41 +0530

    swapped chat alignment

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import {User} from '../shared/user';
 
@@ -9,12 +10,23 @@ import {User} from '../shared/user';
 export class HomeComponent implements OnInit {
 
   public user:User;
-  constructor(user:User) {
+  private _router: Router;
+
+  constructor(user:User, router: Router) {
     this.user=user;
+    this._router = router;
   }
 
   ngOnInit() {
     //alert(this.user.username);
+    if(this.user.username === undefined){
+      this._router.navigateByUrl('login');
+    }
   }
 
+  onLogout(){
+    this.user.username=undefined;
+    this.user.password=undefined;
+    this._router.navigateByUrl('login');
+  }
 }
